0072457b50d3318eb86ee1f38bb920ec2149337f
Deyan Sirakov <103333790+Deyan2306@users.noreply.github.com>
2022-05-22 13:25:39 +0300

    Add files via upload

---
+++ Fund-Java/out/production/Fund-Java/basicSyntaxMoreExercise/ReverseaString.java
@@ -0,0 +1,13 @@
+package basicSyntaxMoreExercise;
+
+import java.util.Scanner;
+
+public class ReverseaString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String reversed = new StringBuffer(input).reverse().toString();
+
+        System.out.println(reversed);
+    }
+}
