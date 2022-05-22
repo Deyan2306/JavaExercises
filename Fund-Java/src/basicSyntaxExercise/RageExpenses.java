5add81cf230d3b9d6b6297cc89af532d54ccf631
Deyan Sirakov <103333790+Deyan2306@users.noreply.github.com>
2022-05-22 11:55:10 +0300

    Add files via upload

---
+++ Fund-Java/src/basicSyntaxExercise/RageExpenses.java
@@ -0,0 +1,25 @@
+package basicSyntaxExercise;
+
+import java.util.Scanner;
+
+public class RageExpenses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int gamesLost = Integer.parseInt(scanner.nextLine());
+        double headsetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboardPrice = Double.parseDouble(scanner.nextLine());
+        double monitorPrice = Double.parseDouble(scanner.nextLine());
+
+        int headsetNum = gamesLost / 2;
+        int mouseNum = gamesLost / 3;
+        int keyboardNum = gamesLost / 6;
+        int monitorNum = gamesLost / 12;
+
+        double totalSum = (headsetNum * headsetPrice)
+                + (monitorNum * monitorPrice)
+                + (mouseNum * mousePrice)
+                + (keyboardNum * keyboardPrice);
+        System.out.printf("Rage expenses: %.2f lv.", totalSum);
+    }
+}
