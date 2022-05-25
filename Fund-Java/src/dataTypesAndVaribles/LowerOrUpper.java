5aae6d7b402b71bcbd7410a0873aac994457440c
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-05-25 11:36:20 -0700

    LowerOrUpper done

---
+++ Fund-Java/src/dataTypesAndVaribles/LowerOrUpper.java
@@ -0,0 +1,16 @@
+package dataTypesAndVaribles;
+
+import java.util.Scanner;
+
+public class LowerOrUpper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char input = scanner.nextLine().charAt(0);
+
+        if (input >= 'a' && input <= 'z') {
+            System.out.println("lower-case");
+        } else if (input >= 'A' && input <= 'Z') {
+            System.out.println("upper-case");
+        }
+    }
+}
