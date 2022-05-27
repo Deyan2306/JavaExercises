fc9fbe8982f3358f7a41a55f034d14e48dd746e9
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-05-27 10:24:52 -0700

    Almost refactored

---
+++ Fund-Java/src/dataTypesAndVaribles/RefactorSpecialNumbers.java
@@ -0,0 +1,27 @@
+package dataTypesAndVaribles;
+
+import java.util.Scanner;
+
+public class RefactorSpecialNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int howMuch = Integer.parseInt(scanner.nextLine());
+        int total = 0;
+        for (int i = 1; i <= howMuch; i++) {
+            int current = i;
+            while (i > 0) {
+                total += i % 10;
+                i /= 10;
+            }
+            boolean isValid = (total == 5) || (total == 7) || (total == 11);
+
+            if (isValid) {
+                System.out.println(i + " -> True");
+            } else {
+                System.out.println(i + " -> False");
+            }
+            total = 0;
+            i = current;
+        }
+    }
+}
