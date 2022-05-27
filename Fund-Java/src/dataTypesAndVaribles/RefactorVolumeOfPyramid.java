3c8d38e13f12849847534caa06a4c18aef03db69
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-05-27 10:15:07 -0700

    Yes, now it should be working

--- Fund-Java/src/dataTypesAndVaribles/RefactorVolumeOfPyramid.java
+++ Fund-Java/src/dataTypesAndVaribles/RefactorVolumeOfPyramid.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class RefactorVolumeOfPyramid {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        double dul, sh, V = 0;
         System.out.print("Length: ");
-        dul = Double.parseDouble(scanner.nextLine());
+        double lenght = Double.parseDouble(scanner.nextLine());
         System.out.print("Width: ");
-        sh = Double.parseDouble(scanner.nextLine());
+        double width = Double.parseDouble(scanner.nextLine());
         System.out.print("Height: ");
-        V = Double.parseDouble(scanner.nextLine());
-        V = (dul * sh * V) / 3;
-        System.out.printf("Pyramid Volume: %.2f", V);
+        double height = Double.parseDouble(scanner.nextLine());
+        double volume = (lenght * width * height) / 3;
+        System.out.printf("Pyramid Volume: %.2f", volume);
     }
 }
