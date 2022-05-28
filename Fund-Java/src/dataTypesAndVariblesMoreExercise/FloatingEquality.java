8c46f7eb9c6a7fe2d3edb7a33fd37da60a9a1922
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-05-28 12:01:05 -0700

    Another Bad Boy Down

---
+++ Fund-Java/src/dataTypesAndVariblesMoreExercise/FloatingEquality.java
@@ -0,0 +1,19 @@
+package dataTypesAndVariblesMoreExercise;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class FloatingEquality {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigDecimal NumOne = new BigDecimal(scanner.nextLine()).setScale(6, RoundingMode.DOWN);
+        BigDecimal NumTwo = new BigDecimal(scanner.nextLine()).setScale(6, RoundingMode.DOWN);
+
+        if (NumOne.equals(NumTwo)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
---
+++ Fund-Java/src/dataTypesAndVariblesMoreExercise/RefactoringPrimeChecker.java
@@ -0,0 +1,4 @@
+package dataTypesAndVariblesMoreExercise;
+
+public class RefactoringPrimeChecker {
+}
