7c18b8c7b9b15f8feda638a5756a16307c73bb5f
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-05-29 04:14:53 -0700

    repainting done

---
+++ PB-Java/src/firstStepsInCodingExercise/repainting.java
@@ -0,0 +1,22 @@
+package firstStepsInCodingExercise;
+
+import java.util.Scanner;
+
+public class repainting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int plastic = Integer.parseInt(scanner.nextLine());
+        int paint = Integer.parseInt(scanner.nextLine());
+        int chemical = Integer.parseInt(scanner.nextLine());
+        int hours = Integer.parseInt(scanner.nextLine());
+
+        double plasticPrice = (plastic + 2) * 1.50;
+        double paintPrice = (paint + (paint * 0.10)) * 14.50;
+        double chemicalPrice = chemical * 5.00;
+        double total = plasticPrice + paintPrice + chemicalPrice + 0.40;
+        double totalWork = (total * 0.30) * hours;
+        double sum = total + totalWork;
+
+        System.out.printf("%.2f", sum);
+    }
+}
