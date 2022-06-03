e414a9ed5be017e7bce661bd4d15410031a5f315
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-06-03 12:03:22 -0700

    Equal Arrays ready :)

---
+++ Fund-Java/src/arraysLab/EqualArrays.java
@@ -0,0 +1,23 @@
+package arraysLab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EqualArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] firstNumCollection = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] secondNumCollection = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sum = 0;
+        for (int i = 0; i < firstNumCollection.length; i++) {
+            if (firstNumCollection[i] == secondNumCollection[i]){
+                sum += firstNumCollection[i];
+            } else {
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                return;
+            }
+        }
+        System.out.printf("Arrays are identical. Sum: %d", sum);
+    }
+}
