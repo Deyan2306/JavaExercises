90debe2da1f2cbf7f4e8362822f77c1ec88a9393
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-06-03 11:48:15 -0700

    Some changes on the reverse strings exercise

--- Fund-Java/src/arraysLab/ReverseArrayOfStrings.java
+++ Fund-Java/src/arraysLab/ReverseArrayOfStrings.java
@@ -8,8 +8,15 @@ public class ReverseArrayOfStrings {
         Scanner scanner = new Scanner(System.in);
         String[] arrInput = scanner.nextLine().split(" ");
 
-        for (int i = arrInput.length - 1; i >=  0; i--) {
-            System.out.print(arrInput[i] + " ");
+        for (int i = 0; i < arrInput.length / 2; i++) {
+            String oldEl = arrInput[i];
+            arrInput[i] = arrInput[arrInput.length - 1 - i];
+            arrInput[arrInput.length - 1 - i] = oldEl;
         }
+        System.out.println(String.join(" ", arrInput));
     }
+
+    //        for (int i = arrInput.length - 1; i >=  0; i--) {
+    //            System.out.print(arrInput[i] + " ");
+    //        }
 }
