fe4f84377b680f291c1a35ab70b94c9a3687285e
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-06-05 02:46:35 -0700

    Magic Sum Ready

---
+++ Fund-Java/src/arraysExercise/MagicSum.java
@@ -0,0 +1,25 @@
+package arraysExercise;
+
+        import java.util.Arrays;
+        import java.util.Scanner;
+
+public class MagicSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long[] allNumbers = Arrays.stream(scanner.nextLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long specialNumber = Long.parseLong(scanner.nextLine());
+
+        for (int i = 0; i < allNumbers.length; i++) {
+
+            for (int j = i + 1; j < allNumbers.length; j++) {
+                if (allNumbers[i] == allNumbers[j]){
+                    break;
+                }
+
+                if (allNumbers[i] + allNumbers[j] == specialNumber){
+                    System.out.println(allNumbers[i] + " " + allNumbers[j]);
+                }
+            }
+        }
+    }
+}
