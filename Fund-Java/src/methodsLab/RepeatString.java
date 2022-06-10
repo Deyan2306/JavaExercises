10234c58f454e07227d3a990a954776980412686
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-06-10 12:47:30 -0700

    Repeat String

---
+++ Fund-Java/src/methodsLab/RepeatString.java
@@ -0,0 +1,21 @@
+package methodsLab;
+
+import java.util.Scanner;
+
+public class RepeatString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String textInput = scanner.nextLine();
+        int repeat = Integer.parseInt(scanner.nextLine());
+        String result = repeatDaString(textInput, repeat);
+        System.out.println(result);
+    }
+
+    public static String repeatDaString (String repeatableString, int rotations){
+        String result = "";
+        for (int i = 0; i < rotations; i++) {
+            result += repeatableString;
+        }
+        return result;
+    }
+}
