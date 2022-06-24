3b93d0d27a53b121c55679957c9d3ef36e45975f
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-06-24 12:57:28 -0700

    bla bla

---
+++ Fund-Java/src/listLab/MergingLists.java
@@ -0,0 +1,4 @@
+package listLab;
+
+public class MergingLists {
+}
---
+++ Fund-Java/src/listLab/SumAdjacentEqualNumbers.java
@@ -0,0 +1,36 @@
+package listLab;
+
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SumAdjacentEqualNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Double> integerList = Arrays.stream(scanner.nextLine().split("\\s+")).map(Double::parseDouble).collect(Collectors.toList());
+
+        for (int i = 0; i < integerList.size() - 1; i++) {
+            double currentNum = integerList.get(i);
+            double nextNum = integerList.get(i + 1);
+
+            if (currentNum == nextNum){
+                integerList.set(i, currentNum + nextNum);
+                integerList.remove(i + 1);
+
+                i = -1;
+            }
+        }
+
+        String output = joinByDelimiter(integerList, " ");
+        System.out.println(output);
+
+    }
+
+    public static String joinByDelimiter(List<Double> items, String delimiter){
+        String output = "";
+        for (Double item : items){
+            output += (new DecimalFormat("0.#").format(item) + delimiter);
+        }
+        return output;
+    }
+}
