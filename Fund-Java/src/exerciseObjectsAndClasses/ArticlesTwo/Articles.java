fe6277e3e50e8278e01c51ba518230009b5605f6
Deyan2306 <deyan.sirakov2006@abv.bg>
2022-07-11 00:57:46 -0700

    Articles Two

---
+++ Fund-Java/src/exerciseObjectsAndClasses/ArticlesTwo/Articles.java
@@ -0,0 +1,30 @@
+package exerciseObjectsAndClasses.ArticlesTwo;
+
+public class Articles {
+    private String title;
+    private String content;
+    private String author;
+
+    public Articles (String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s: %s", title, content, author);
+    }
+}
